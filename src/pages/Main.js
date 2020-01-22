4514c48bf9daf7082bd6fd032c2a45528bd41852
Iarlen Aquiles <iarlensilva775@gmail.com>
2020-01-22 14:57:34 -0300

    criando o map com os devs listados

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,8 +1,87 @@
-import React from "react";
-import { View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Image, View, Text } from "react-native";
+import MapView, { Marker, Callout } from "react-native-maps";
+import {
+  requestPermissionsAsync,
+  getCurrentPositionAsync
+} from "expo-location";
 
 function Main() {
-  return <View></View>;
+  const [currentRegion, setCurrentRegion] = useState(null);
+
+  useEffect(() => {
+    async function loadInitialPosition() {
+      const { granted } = await requestPermissionsAsync();
+
+      if (granted) {
+        const { coords } = await getCurrentPositionAsync({
+          enableHighAccuracy: true
+        });
+
+        const { latitude, longitude } = coords;
+        setCurrentRegion({
+          latitude,
+          longitude,
+          latitudeDelta: 0.04,
+          longitudeDelta: 0.04
+        });
+      }
+    }
+
+    loadInitialPosition();
+  }, []);
+
+  if (!currentRegion) {
+    return null;
+  }
+
+  return (
+    <MapView initialRegion={currentRegion} style={styles.map}>
+      <Marker coordinate={{ latitude: -3.770753, longitude: -38.628851 }}>
+        <Image
+          source={{
+            uri: "https://avatars0.githubusercontent.com/u/3675462?s=460&v=4"
+          }}
+          style={styles.avatar}
+        />
+        <Callout>
+          <View style={styles.callout}>
+            <Text style={styles.devName}>Iarlen</Text>
+            <Text style={styles.devBio}>bio</Text>
+            <Text style={styles.devTechs}>techs</Text>
+          </View>
+        </Callout>
+      </Marker>
+    </MapView>
+  );
 }
+
+const styles = StyleSheet.create({
+  map: {
+    flex: 1
+  },
+  avatar: {
+    width: 54,
+    height: 54,
+    borderRadius: 4,
+    borderWidth: 4,
+    borderColor: "#fff"
+  },
+  callout: {
+    width: 260,
+    margin: 5
+  },
+  devName: {
+    fontWeight: "bold",
+    fontSize: 16
+  },
+  devBio: {
+    color: "#666",
+    marginTop: 5
+  },
+  devTechs: {
+    marginTop: 5
+  }
+});
 
 export default Main;
