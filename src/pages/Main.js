5765ddd3e79ca50c025dc9e602013db61dfcc8ac
Iarlen Aquiles <iarlensilva775@gmail.com>
2020-01-22 15:34:19 -0300

    criando input para buscar os devs

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,10 +1,19 @@
 import React, { useEffect, useState } from "react";
-import { StyleSheet, Image, View, Text } from "react-native";
+import {
+  StyleSheet,
+  Image,
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity
+} from "react-native";
 import MapView, { Marker, Callout } from "react-native-maps";
 import {
   requestPermissionsAsync,
   getCurrentPositionAsync
 } from "expo-location";
+
+import { MaterialIcons } from "@expo/vector-icons";
 
 function Main({ navigation }) {
   const [currentRegion, setCurrentRegion] = useState(null);
@@ -36,29 +45,45 @@ function Main({ navigation }) {
   }
 
   return (
-    <MapView initialRegion={currentRegion} style={styles.map}>
-      <Marker coordinate={{ latitude: -3.770753, longitude: -38.628851 }}>
-        <Image
-          source={{
-            uri: "https://avatars0.githubusercontent.com/u/3675462?s=460&v=4"
-          }}
-          style={styles.avatar}
+    <>
+      <MapView initialRegion={currentRegion} style={styles.map}>
+        <Marker coordinate={{ latitude: -3.770753, longitude: -38.628851 }}>
+          <Image
+            source={{
+              uri: "https://avatars0.githubusercontent.com/u/3675462?s=460&v=4"
+            }}
+            style={styles.avatar}
+          />
+          <Callout
+            onPress={() => {
+              navigation.navigate("Profile", {
+                github_username: "diego3g"
+              });
+            }}
+          >
+            <View style={styles.callout}>
+              <Text style={styles.devName}>Iarlen</Text>
+              <Text style={styles.devBio}>bio</Text>
+              <Text style={styles.devTechs}>techs</Text>
+            </View>
+          </Callout>
+        </Marker>
+      </MapView>
+
+      <View style={styles.searchForm}>
+        <TextInput
+          style={styles.searchInput}
+          placeholder="Buscar devs por techs..."
+          placeholderTextColor="#999"
+          autoCapitalize="words"
+          autoCorrect={false}
         />
-        <Callout
-          onPress={() => {
-            navigation.navigate("Profile", {
-              github_username: "diego3g"
-            });
-          }}
-        >
-          <View style={styles.callout}>
-            <Text style={styles.devName}>Iarlen</Text>
-            <Text style={styles.devBio}>bio</Text>
-            <Text style={styles.devTechs}>techs</Text>
-          </View>
-        </Callout>
-      </Marker>
-    </MapView>
+
+        <TouchableOpacity onPress={() => {}} style={styles.loadButton}>
+          <MaterialIcons name="my-location" size={20} color="#fff" />
+        </TouchableOpacity>
+      </View>
+    </>
   );
 }
 
@@ -87,6 +112,39 @@ const styles = StyleSheet.create({
   },
   devTechs: {
     marginTop: 5
+  },
+  searchForm: {
+    position: "absolute",
+    top: 20,
+    left: 20,
+    right: 20,
+    zIndex: 5,
+    flexDirection: "row"
+  },
+  searchInput: {
+    flex: 1,
+    height: 50,
+    backgroundColor: "#FFF",
+    color: "#333",
+    borderRadius: 25,
+    paddingHorizontal: 20,
+    fontSize: 16,
+    shadowColor: "#000",
+    shadowOpacity: 0.2,
+    shadowOffset: {
+      width: 4,
+      height: 4
+    },
+    elevation: 2
+  },
+  loadButton: {
+    width: 50,
+    height: 50,
+    backgroundColor: "#8e4dff",
+    borderRadius: 25,
+    justifyContent: "center",
+    alignItems: "center",
+    marginLeft: 15
   }
 });
 
